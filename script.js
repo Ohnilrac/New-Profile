c9897e5987733f9efa4a0b9993167a9439cb6650
Jonas Nunes <jonasctps@gmail.com>
2022-10-21 23:34:45 -0300

    Botao dark mode e minimizar menu

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const body = document.querySelector('body'),
   sidebar = body.querySelector('.sidebar'),
+  toggle = body.querySelector('.toggle'),
   modeSwitch = body.querySelector('.toggle-switch'),
   modeText = body.querySelector('.mode-text')
 
-modeSwitch.addEventListener("click", () => {
+toggle.addEventListener('click', () => {
+  sidebar.classList.toggle('close')
+})
+
+modeSwitch.addEventListener('click', () => {
   body.classList.toggle('dark')
+
+  if (body.classList.contains('dark')) {
+    modeText.innerHTML = 'Modo Claro'
+  } else {
+    modeText.innerHTML = 'Modo Escuro'
+  }
 })
